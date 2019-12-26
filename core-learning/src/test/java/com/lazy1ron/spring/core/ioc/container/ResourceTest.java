2120cb5be5f3078cb2b5c1b5d4ad502b6a919bda
Lazy1ron <lazy1ron.s3efun@gmail.com>
2019-12-26 10:12:52 +0800

    use xml metadata to initialize a context

--- core-learning/src/test/java/com/lazy1ron/spring/core/ioc/container/ResourceTest.java
+++ core-learning/src/test/java/com/lazy1ron/spring/core/ioc/container/ResourceTest.java
@@ -1,7 +1,10 @@
 package com.lazy1ron.spring.core.ioc.container;
 
+import com.lazy1ron.spring.resource.service.EasyService;
+import com.lazy1ron.spring.resource.service.ForeignService;
 import org.junit.Test;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.util.CollectionUtils;
 
@@ -33,5 +36,20 @@ public class ResourceTest {
     @Test
     public void test2() {
         System.out.println(System.getProperty("spring.application.name"));
+    }
+
+
+    // use xml metadata to initialize a context
+    @Test
+    public void test3() {
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("classpath*:xml/beans.xml");
+
+        EasyService easyService = context.getBean(EasyService.class);
+        easyService.sayHello();
+
+        ForeignService fs = (ForeignService) context.getBean("foreignService");
+        fs.sayHello();
+
+
     }
 }
